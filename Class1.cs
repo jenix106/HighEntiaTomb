440d62072509e7447c5fe8457d3194fd0ea3b72e
Ian Hooey <ian.hooey@gmail.com>
2022-08-16 18:10:24 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThunderRoad;
+using UnityEngine;
+
+namespace HighEntiaTomb
+{
+    public class HighEntiaTombModule : LevelModule
+    {
+        AudioSource battleMusic;
+        AudioSource uniqueBattleMusic;
+        AudioSource music;
+        bool isInCombat;
+        public override IEnumerator OnLoadCoroutine()
+        {
+            battleMusic = level.customReferences.Find(match => match.name == "Music").transforms.Find(match => match.name == "BattleMusic").GetComponent<AudioSource>();
+            uniqueBattleMusic = level.customReferences.Find(match => match.name == "Music").transforms.Find(match => match.name == "UniqueBattleMusic").GetComponent<AudioSource>();
+            music = level.customReferences.Find(match => match.name == "Music").transforms.Find(match => match.name == "Music").GetComponent<AudioSource>();
+            foreach(Transform transform in level.customReferences.Find(match => match.name == "Climbable").transforms)
+            {
+                transform.gameObject.AddComponent<ClimbableComponent>();
+            }
+            return base.OnLoadCoroutine();
+        }
+        public override void Update()
+        {
+            base.Update();
+            isInCombat = false;
+            foreach (Creature creature in Creature.allActive)
+            {
+                if (creature?.brain?.currentTarget != null && !creature.isPlayer && !creature.isKilled && (creature.brain.currentTarget.isPlayer || creature.brain.currentTarget.faction == Player.local.creature.faction))
+                {
+                    isInCombat = true;
+                    if (battleMusic.isPlaying) return;
+                    battleMusic.Play();
+                    music.Pause();
+                }
+            }
+            if (isInCombat || !battleMusic.isPlaying) return;
+            uniqueBattleMusic.Stop();
+            battleMusic.Stop();
+            battleMusic.mute = false;
+            music.UnPause();
+        }
+    }
+    public class ClimbableComponent : MonoBehaviour
+    {
+        public void OnCollisionStay(Collision c)
+        {
+            if (c.collider.GetComponentInParent<Player>() != null)
+            {
+                RagdollHandClimb.climbFree = true;
+            }
+        }
+        public void OnCollisionExit(Collision c)
+        {
+            if (c.collider.GetComponentInParent<Player>() != null)
+            {
+                RagdollHandClimb.climbFree = false;
+            }
+        }
+    }
+}
---
+++ XCRPG.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThunderRoad;
+using UnityEngine;
+using XenobladeRPG;
+
+namespace HighEntiaTomb
+{
+    class XCRPGModule : LevelModule
+    {
+        AudioSource battleMusic;
+        AudioSource uniqueBattleMusic;
+        bool isFightingUnique;
+        public override IEnumerator OnLoadCoroutine()
+        {
+            battleMusic = level.customReferences.Find(match => match.name == "Music").transforms.Find(match => match.name == "BattleMusic").GetComponent<AudioSource>();
+            uniqueBattleMusic = level.customReferences.Find(match => match.name == "Music").transforms.Find(match => match.name == "UniqueBattleMusic").GetComponent<AudioSource>();
+            return base.OnLoadCoroutine();
+        }
+        public override void Update()
+        {
+            base.Update();
+            isFightingUnique = false;
+            foreach (Creature creature in Creature.allActive)
+            {
+                if (creature?.brain?.currentTarget != null && !creature.isPlayer && !creature.isKilled && (creature.brain.currentTarget == Player.local.creature || creature.brain.currentTarget.faction == Player.local.creature.faction))
+                {
+                    if (creature.GetComponent<XenobladeStats>() != null && creature.GetComponent<XenobladeStats>().isUnique)
+                    {
+                        isFightingUnique = true;
+                        if (uniqueBattleMusic.isPlaying) return;
+                        uniqueBattleMusic.Play();
+                        battleMusic.mute = true;
+                    }
+                }
+            }
+            if (isFightingUnique || !uniqueBattleMusic.isPlaying) return;
+            uniqueBattleMusic.Stop();
+            battleMusic.mute = false;
+        }
+    }
+}
